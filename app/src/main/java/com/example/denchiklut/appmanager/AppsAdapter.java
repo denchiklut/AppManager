27cb8feaf45435e8ef7f18efb1e01970618ed52e
Denis <denchiklut@gmail.com>
2018-08-18 18:55:08 -0700

    Список приложений выводится на главный экран

---
+++ app/src/main/java/com/example/denchiklut/appmanager/AppsAdapter.java
@@ -0,0 +1,76 @@
+package com.example.denchiklut.appmanager;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppsAdapter extends RecyclerView.Adapter<AppsAdapter.ViewHolder> {
+
+    private List<AppInfo> apps = new ArrayList<>();
+
+    public void setApps (List<AppInfo> apps) {
+        this.apps = apps;
+    }
+
+    @NonNull
+    // В этом методе мы создаем новую ячейку
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        Context context = viewGroup.getContext();
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+
+        View view = layoutInflater.inflate(R.layout.view_item_app, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    // В этом методе мы привязываем данные к ячейке
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+        AppInfo appInfo = apps.get(i);
+
+        viewHolder.nameTv.setText(appInfo.getName());
+        viewHolder.versionTv.setText(appInfo.getVersionName());
+        viewHolder.dirTv.setText(appInfo.getDir());
+        viewHolder.iconIv.setImageDrawable(appInfo.getIcon());
+
+    }
+
+    // В этом методе мы возвращаем количество элементов списка
+    @Override
+    public int getItemCount() {
+        return apps.size();
+    }
+
+
+    /**
+     * View holder
+     * Хранит информацию о ячейке
+     */
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView iconIv;
+        private final TextView nameTv;
+        private final TextView versionTv;
+        private final TextView dirTv;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            iconIv = itemView.findViewById(R.id.icon_iv);
+            nameTv = itemView.findViewById(R.id.name_tv);
+            versionTv = itemView.findViewById(R.id.version_tv);
+            dirTv = itemView.findViewById(R.id.dir_tv);
+        }
+    }
+
+}
--- app/src/main/java/com/example/denchiklut/appmanager/MainActivity.java
+++ app/src/main/java/com/example/denchiklut/appmanager/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.denchiklut.appmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import java.util.List;
@@ -19,9 +21,13 @@ public class MainActivity extends AppCompatActivity {
         appManager = new AppManager(this);
 
         List<AppInfo> installedApps = appManager.getInstalledApps();
+        AppsAdapter adapter =new AppsAdapter();
 
-        for (AppInfo info: installedApps) {
-            Log.i(TAG, "App " + info);
-        }
+        RecyclerView recyclerView = findViewById(R.id.apps_rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        adapter.setApps(installedApps);
+        adapter.notifyDataSetChanged();
     }
 }
